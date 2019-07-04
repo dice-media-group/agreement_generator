9df6f928927a458972f4aad76abaebf2ca64f9b6
captproton <rocketeer.captproton@gmail.com>
2019-07-04 09:35:00 -0700

    INIT: section_scaffold

---
+++ app/views/sections/_section.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! section, :id, :title, :body, :boilerplate_text, :created_at, :updated_at
+json.url section_url(section, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :sections
   resources :deliverables
   resources :projects
   resources :agreements
