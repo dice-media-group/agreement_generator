61a003f1edfbf9584a9a63e7a5b8279a888712d0
captproton <rocketeer.captproton@gmail.com>
2019-07-28 19:57:17 -0700

    FIX: attempt to fix email not sending

--- app/models/approval_request.rb
+++ app/models/approval_request.rb
@@ -1,4 +1,16 @@
 class ApprovalRequest < ApplicationRecord
   belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
   belongs_to :agreement
+
+  before_validation :email_request, if: :recipient_id?
+
+  def email_request
+  	Rails.logger.info "Attempting rails ApprovalRequest.email_request"
+  	recipient = User.find(self.recipient_id)
+  	agreement = Agreement.find(self.agreement_id)
+  	Rails.logger.info "agreement: #{agreement}, recipient: #{recipient}"
+
+	message 	= UserMailer.request_agreement_approval_email(recipient, agreement)
+  end
+
 end
