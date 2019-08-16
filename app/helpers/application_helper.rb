335819c6934708683ce86b4d7ed5f7afe170c3bd
captproton <rocketeer.captproton@gmail.com>
2019-08-15 20:16:55 -0700

    ADDED: dynamic title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
       notice: "alert-info"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
+
+ def full_title(page_title)
+  base_title = "Dice Media HQ"
+  page_title.empty? ?  base_title : "#{base_title} | #{page_title}"
+ end
+
 end
